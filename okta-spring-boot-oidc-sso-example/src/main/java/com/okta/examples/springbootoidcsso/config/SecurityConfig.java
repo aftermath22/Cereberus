a3ce5450524f0158931d27c8e28951983e161d63
Keval <kevalshah102@gmail.com>
2025-10-11 08:00:49 +0530

    refine logout flow in task manager and some ui changes

--- okta-spring-boot-oidc-sso-example/src/main/java/com/okta/examples/springbootoidcsso/config/SecurityConfig.java
+++ okta-spring-boot-oidc-sso-example/src/main/java/com/okta/examples/springbootoidcsso/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.okta.examples.springbootoidcsso.config;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,17 +14,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -32,34 +29,8 @@ public class SecurityConfig {
     @Value("${okta.oauth2.issuer}")
     private String issuer;
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromOidcIssuerLocation(issuer);
-    }
-
-    @Bean
-    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            authorities.forEach(authority -> {
-                if (authority instanceof OidcUserAuthority) {
-                    OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
-                    OidcIdToken idToken = oidcUserAuthority.getIdToken();
-                    List<String> groups = idToken.getClaimAsStringList("groups");
-
-                    if (groups != null) {
-                        groups.forEach(group ->
-                            mappedAuthorities.add(new SimpleGrantedAuthority(group))
-                        );
-                    }
-                }
-                mappedAuthorities.add(authority);
-            });
-
-            return mappedAuthorities;
-        };
-    }
+    // @Value("${okta.oauth2.client-id}")
+    // private String clientId;
 
     // This configuration is for the API endpoints
     @Configuration
@@ -78,7 +49,12 @@ public class SecurityConfig {
 
     // This configuration is for the user-facing web pages
     @Configuration
-    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+            return new OidcLogoutSuccessHandler();
+        }
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
@@ -86,9 +62,38 @@ public class SecurityConfig {
                     .antMatchers("/", "/css/**", "/js/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
-                .oauth2Login() // Use the standard login flow
+                    .oauth2Login() // Use the standard login flow
                 .and()
-                .logout();
+                    .logout()
+                    .logoutSuccessHandler(oidcLogoutSuccessHandler());
         }
+    }
+
+    // Inner class to handle Okta logout
+    private class OidcLogoutSuccessHandler implements LogoutSuccessHandler {
+
+        @Override
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            String idToken = null;
+            if (authentication instanceof OAuth2AuthenticationToken) {
+                OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+                if (oauthToken.getPrincipal() instanceof DefaultOidcUser) {
+                    idToken = ((DefaultOidcUser) oauthToken.getPrincipal()).getIdToken().getTokenValue();
+                }
+            }
+
+            // Build the logout URL
+            String logoutUrl = issuer + "/v1/logout?id_token_hint=" +
+                URLEncoder.encode(idToken, StandardCharsets.UTF_8.name()) +
+                "&post_logout_redirect_uri=" +
+                URLEncoder.encode("http://localhost:8080/", StandardCharsets.UTF_8.name());
+
+            response.sendRedirect(logoutUrl);
+        }
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromOidcIssuerLocation(issuer);
     }
 }
