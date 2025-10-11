bd3c15b4ed88540f5253c3f5874943e3408df1d9
Keval <kevalshah102@gmail.com>
2025-10-11 10:26:10 +0530

    task changes and dockerfile upd

--- okta-spring-boot-oidc-sso-example/src/main/java/com/okta/examples/springbootoidcsso/task/Task.java
+++ okta-spring-boot-oidc-sso-example/src/main/java/com/okta/examples/springbootoidcsso/task/Task.java
@@ -1,26 +1,33 @@
 package com.okta.examples.springbootoidcsso.task;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
+@Data
 @Entity
 public class Task {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String description;
-    private String date;
-    private String userId; // To associate the task with a user
 
-    // Getters and Setters for all fields
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userId;
+    private String text;
+    private Date created;
+
+    public Task() {}
+
+    public Task(String userId, String text, Date created) {
+        this.userId = userId;
+        this.text = text;
+        this.created = created;
+    }
+
+    // Explicitly add the setText method
+    public void setText(String text) {
+        this.text = text;
+    }
 }
